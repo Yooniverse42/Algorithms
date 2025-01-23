d4378aec7bdb3700ea22d54b6992313e62d12ef4
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-01-23 10:39:38 +0900

    16. Star pattern 3(콤마는 공백으로 인식)

--- 프로그래머스/16.Star_pattern_3/index.js
+++ 프로그래머스/16.Star_pattern_3/index.js
@@ -1,7 +1,7 @@
 const star3 = (n) => {
   let star = "*";
   for (let i = 1; i <= n; i++) {
-    console.log(" ".repeat(n - i), star.repeat(i));
+    console.log(" ".repeat(n - i) + star.repeat(i)); // 콤마가 아닌 플러스로 문자열 연결해야함.
   }
 };
 

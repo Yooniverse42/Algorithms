cc750cce22b1360bee734d171502774562424c43
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-02-20 15:15:26 +0900

    58. 글자 이어 붙여 문자열 만들기

---
+++ 프로그래머스/Day6/58.글자_이어_붙여_문자열_만들기/index.js
@@ -0,0 +1,8 @@
+const solution = (str, indices) => indices.map((item) => str[item]).join("");
+
+console.log(
+  solution("cvsgiorszzzmrpaqpe", [16, 6, 5, 3, 12, 14, 11, 11, 17, 12, 7])
+);
+// 'programmers'
+console.log(solution("zpiaz", [1, 2, 0, 0, 3]));
+// 'pizza'

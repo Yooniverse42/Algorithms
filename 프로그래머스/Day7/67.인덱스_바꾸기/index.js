d64e2de7e4c4056b19165238c34b28694fdc2b64
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-03-03 20:50:50 +0900

    67. 인덱스 바꾸기

--- 프로그래머스/Day7/67.인덱스_바꾸기/index.js
+++ 프로그래머스/Day7/67.인덱스_바꾸기/index.js
@@ -1,5 +1,9 @@
 const solution = (str, num1, num2) => {
-  // do something
+  let arr = [...str];
+  arr[num1] = str[num2];
+  arr[num2] = str[num1];
+  // [arr[num1], arr[num2]] = [arr[num2], arr[num1]];
+  return arr.join("");
 };
 
 console.log(solution("hello", 1, 2)); // 'hlelo'

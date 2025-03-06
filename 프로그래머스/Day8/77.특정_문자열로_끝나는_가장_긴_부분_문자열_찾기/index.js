66866e167f8ce602abfe280348753ab5cd5c193b
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-03-06 15:07:50 +0900

    77. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기

--- 프로그래머스/Day8/77.특정_문자열로_끝나는_가장_긴_부분_문자열_찾기/index.js
+++ 프로그래머스/Day8/77.특정_문자열로_끝나는_가장_긴_부분_문자열_찾기/index.js
@@ -1,25 +1,22 @@
-const sumMatrix = (matrix) =>
-  matrix.map((i) => i.reduce((a, b) => a + b, 0)).reduce((c, d) => c + d, 0);
+const solution = (str, part) => {
+  const res = [];
+  for (let i = 0; i < str.length; i++) {
+    const newStr = str.slice(0, i + 1);
 
-console.log(
-  sumMatrix([
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9],
-  ])
-); // 45
-console.log(sumMatrix([])); // 0
-console.log(
-  sumMatrix([
-    [10, -10],
-    [20, 30],
-    [5, 5],
-  ])
-); // 60
+    if (newStr.includes(part)) {
+      res.push(newStr);
 
-// Another Solution 1
-// const sumMatrix = (matrix) =>
-//   matrix.reduce((sum, row) => sum + row.reduce((a, b) => a + b, 0), 0);
+      const addStr = str.slice(i + 1, str.length);
+      if (addStr.includes(part)) continue;
+      else break;
+    }
+  }
 
-// Another Solution 2
-// const sumMatrix = matrix => matrix.flat().reduce((sum, num) => sum + num, 0);
+  return res.at(-1);
+};
+
+console.log(solution("AbCdEFG", "dE")); // 'AbCdE'
+console.log(solution("AAAAaaaa", "a")); // 'AAAAaaaa'
+
+// Another Solution
+// const solution = (str, part) => str.substring(0, str.lastIndexOf(part)) + part;

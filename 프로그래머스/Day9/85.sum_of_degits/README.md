5fd3a8bea2d5d2bacdd1ee0b4307b28782fa2fcf
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-03-12 10:54:25 +0900

    85. sum of degits

---
+++ 프로그래머스/Day9/85.sum_of_degits/README.md
@@ -0,0 +1,14 @@
+양의 정수(1 ≤ n ≤ 100)를 전달받아 각 자릿수의 합을 반환한다.
+
+- 숫자를 문자열로 변환하는 방법은 사용하지 않는다.
+
+```jsx
+const sumOfDigits = (n) => {
+  // do something
+};
+
+console.log(sumOfDigits(1)); // 1
+console.log(sumOfDigits(123)); // 6
+console.log(sumOfDigits(1010)); // 2
+console.log(sumOfDigits(54321)); // 15
+```
---
+++ 프로그래머스/Day9/85.sum_of_degits/index.js
@@ -0,0 +1,18 @@
+const sumOfDigits = (n) => {
+  let sum = 0;
+
+  while (n > 0) {
+    sum += n % 10;
+    console.log("n % 10 :", n % 10);
+
+    console.log("floor:", n / 10);
+    n = Math.floor(n / 10);
+  }
+
+  return sum;
+};
+
+console.log(sumOfDigits(1)); // 1
+console.log(sumOfDigits(123)); // 6
+console.log(sumOfDigits(1010)); // 2
+console.log(sumOfDigits(54321)); // 15

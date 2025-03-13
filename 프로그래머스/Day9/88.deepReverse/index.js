850f98f57fef20e684edfbaaf657c10f1a4dea89
Yooniverse42 <162732401+Yooniverse42@users.noreply.github.com>
2025-03-13 23:16:41 +0900

    88. deepReverse

---
+++ 프로그래머스/Day9/88.deepReverse/index.js
@@ -0,0 +1,10 @@
+const deepReverse = (arr) => {
+  if (!Array.isArray(arr)) return arr;
+
+  return arr.map(deepReverse).reverse();
+};
+
+console.log(deepReverse([1, [2, [4, 5]]])); // [[[[5, 4], 3], 2], 1]
+console.log(deepReverse(1)); // 1
+console.log(deepReverse(null)); // null
+console.log(deepReverse()); // undefined
